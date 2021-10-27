d9512ed214939c91817f4fc83553d0fa7bde0c10
MariFon <87157578+MariFon@users.noreply.github.com>
2021-10-26 22:07:22 -0300

    site com banner rotativo
    com jquery nivo slider e slick

---
+++ jquery3/js/app.js
@@ -0,0 +1,13 @@
+$('#lista-banners').nivoSlider({
+    controlNavThumbs: true
+})
+
+$('#lista-carros').nivoSlider({
+    controlNavThumbs: true
+})
+
+$('#lista-lanches').slick({
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    infinite: true
+})
